abc42702691994762d7cac3702bbaaed0ebaf58d
Mihail <33730411+MihailAndreyev@users.noreply.github.com>
2022-09-28 23:55:15 +0300

    Update Simple.c

--- Simple.c
+++ Simple.c
@@ -1,29 +1,37 @@
 #include <stdio.h>
 
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int a,i;
+    
     printf("Vvedite chislo: \n");
-    scanf("%d", &a);
-    if (a=1)
+    scanf_s("%d", &a);
+    if (a == 1)
     {
-       printf("Vvedite chislo: \n");
+        printf("Vvedite chislo: \n");
+        scanf_s("%d", &a);
     }
-    if ((a%2==0 && a==2) || (a%3==0 && a==3))
+    if ((a % 2 == 0 && a == 2) || (a % 3 == 0 && a == 3))
     {
-       printf("A prostoe chislo\n");
+        printf("A prostoe chislo\n");
     }
     else
     {
-        for (i=4;i<a;i++)
+        for (i = 4; i < a; i++)
         {
-           if (a%i==0)
-           {
-               printf("A sostavnoe chislo\n");
-           }
+            if (a % i == 0)
+            {
+                 printf("A sostavnoe chislo\n");
+                 break;
+            }
+        }
+        if  (i == a )
+        {
+            printf("A prostoe chislo\n");
         }
     }
+        
 
-    return 0;
+    return (0);
 }
